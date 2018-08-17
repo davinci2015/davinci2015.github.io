570d40b1481832281a3c8dd3bc28263a7bc654f4
davinci2015 <danijel.vincijanovic@gmail.com>
2018-08-17 14:21:41 +0200

    Site update

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-$(function() {
-	var message = {
+$(function () {
+    var message = {
+        message: [
+            'web development',
+            'eye candy design',
+            'good movie and kokice',
+            'sve što vole mladi;'
+        ],
+        counterS: 0,
+        counterL: 0,
+        deleteS: false,
 
-		message: [
-			'web development;', 
-			'eye candy design;', 
-			'good movie and kokice;', 
-			'sve što vole mladi;'
-		],
-		counterS: 0,
-		counterL: 0,
-		deleteS: false,
+        init: function () {
+            this.cacheElem()
+            this.type()
+        },
 
-		init: function() {
-			this.cacheElem();
-			this.type();
-		},
+        cacheElem: function () {
+            this.$text = $('.text')
+        },
 
-		cacheElem: function() {
-			this.$text = $('.text');
-		},
+        type: function () {
+            var message = this.message[this.counterS],
+                that = this,
+                speed = 0
 
-		type: function() {
-			var 	message 	= this.message[this.counterS],
-				that 	= this,
-				speed 	= 0;
-
-			message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
-			if(this.message[this.counterS] != message && !this.deleteS) {
-				this.$text.text(message);
-				speed = 90;
-			}
-			else {
-				speed = this.message[this.counterS] == message ? 1000 : 40;
-				this.deleteS = true;
-				this.$text.text(message);
-				if (message == '') {
-					this.deleteS = false;
-					this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
-				}
-			}
-			setTimeout(function(){that.type()}, speed);
-		}
-	};
-	message.init();
-});
+            message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL)
+            if (this.message[this.counterS] != message && !this.deleteS) {
+                this.$text.text(message)
+                speed = 60
+            }
+            else {
+                speed = this.message[this.counterS] == message ? 1000 : 30
+                this.deleteS = true
+                this.$text.text(message)
+                if (message == '') {
+                    this.deleteS = false
+                    this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0
+                }
+            }
+            setTimeout(function () {
+                that.type()
+            }, speed)
+        }
+    }
+    message.init()
+})
