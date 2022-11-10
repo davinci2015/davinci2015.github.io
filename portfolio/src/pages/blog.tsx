40867556e37d07470b50e2dfa1cc69748ae150ac
Danijel Vincijanović <danijel.vincijanovic@gmail.com>
2022-11-10 14:36:24 +0100

    Blog page

---
+++ portfolio/src/pages/blog.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogPage = () => <Layout>I'm blog page</Layout>
+
+export const Head = () => <Seo title="Blog" />
+
+export default BlogPage
