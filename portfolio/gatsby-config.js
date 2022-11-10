166b770aeaff9d129ace40fee1297ef920d09427
Danijel Vincijanović <danijel.vincijanovic@gmail.com>
2022-11-10 22:57:49 +0100

    Update

--- portfolio/gatsby-config.js
+++ portfolio/gatsby-config.js
@@ -39,7 +39,19 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
