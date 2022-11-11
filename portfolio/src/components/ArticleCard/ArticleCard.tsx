57178b1ae6ee7411995d141a48dee0627a1c4776
Danijel Vincijanović <danijel.vincijanovic@gmail.com>
2022-11-11 10:55:31 +0100

    Added articles

--- portfolio/src/components/ArticleCard/ArticleCard.tsx
+++ portfolio/src/components/ArticleCard/ArticleCard.tsx
@@ -1,48 +1,62 @@
 import React from "react"
 import { Link } from "gatsby"
-import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image"
+
+import { routes } from "../../routes/routes"
 
 import * as classes from "./style.module.css"
-import { routes } from "../../routes/routes"
+
+const DynamicLink = (props: {
+  url?: string
+  slug?: string
+  children: React.ReactElement
+}) => {
+  if (props.url) {
+    return (
+      <a href={props.url} target="_blank" rel="noopener noreferrer">
+        {props.children}
+      </a>
+    )
+  }
+
+  if (props.slug) {
+    return <Link to={routes.blog(props.slug)}>{props.children}</Link>
+  }
+
+  return null
+}
 
 type Props = {
   title: string
   date: string
   readingTime: string
-  slug: string
-  image: ImageDataLike
+  image: string
+  url?: string
+  slug?: string
 }
 
 export const ArticleCard = ({
   title,
   date,
   readingTime,
-  slug,
   image,
-}: Props) => {
-  const imageSrc = getImage(image)
-
-  return (
-    <Link to={routes.blog(slug)}>
-      <article className={classes.Article}>
-        {imageSrc && (
-          <picture>
-            <GatsbyImage
-              className={classes.ImageWrapper}
-              image={imageSrc}
-              alt={title}
-            />
-          </picture>
-        )}
-        <div className={classes.Description}>
-          <h4>{title}</h4>
-          <div>
-            <p>
-              {date} - <span>{readingTime} min read</span>
-            </p>
-          </div>
+  url,
+  slug,
+}: Props) => (
+  <DynamicLink slug={slug} url={url}>
+    <article className={classes.Article}>
+      {image && (
+        <picture className={classes.ImageWrapper}>
+          <img className={classes.Image} src={image} alt={title} />
+        </picture>
+      )}
+      <div className={classes.Description}>
+        <h4>{title}</h4>
+        <div>
+          <p>
+            {date} - <span>{readingTime} min read</span>
+          </p>
         </div>
-      </article>
-    </Link>
-  )
-}
+      </div>
+    </article>
+  </DynamicLink>
+)
--- portfolio/src/sections/Blog/Blog.tsx
+++ portfolio/src/sections/Blog/Blog.tsx
@@ -1,11 +1,43 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
 
 import { Animation, ArticleCard, Section } from "../../components"
 
 import * as classes from "./style.module.css"
 
+const externalArticles = [
+  {
+    url: "https://medium.cobeisfresh.com/santa-react-patterns-giveaway-bc5bab7c2b9b",
+    image: "https://miro.medium.com/max/1400/1*ylRrUig8T8KHdF3jda-q_Q.png",
+    title: "Santa React patterns giveaway",
+    date: "Dec 10, 2018",
+    readingTime: "3",
+  },
+  {
+    url: "https://medium.com/cobe-mobile/level-up-your-react-architecture-with-mvvm-a471979e3f21",
+    image: "https://miro.medium.com/max/1400/1*n9fsWpNZiynQBBkvYo56uA.webp",
+    title: "Level up your React architecture with MVVM",
+    date: "Aug 30, 2018",
+    readingTime: "7",
+  },
+  {
+    url: "https://medium.cobeisfresh.com/how-redux-can-make-you-a-better-developer-30a094d5e3ec",
+    image: "https://miro.medium.com/max/1400/1*_yEFccUeSpixbI5Q62Ha3Q.jpeg",
+    title: "How Redux Can Make You a Better Developer",
+    date: "Nov 8, 2017",
+    readingTime: "9",
+  },
+  {
+    url: "https://medium.cobeisfresh.com/how-to-win-a-hackathon-tips-tricks-8cd391e18705",
+    image: "https://miro.medium.com/max/1400/1*DjNc34ccCSbbj-qtxW9jHw.webp",
+    title: "How to Win a Hackathon — Tips & Tricks",
+    date: "Apr 13, 2017",
+    readingTime: "10",
+  },
+]
+
 export const Blog = () => {
+  /*
+  Once we start migrating to blog inside the website we can use this query:
   const data = useStaticQuery(graphql`
     query BlogQuery {
       allMdx {
@@ -25,8 +57,7 @@ export const Blog = () => {
       }
     }
   `)
-
-  const articles = data.allMdx.nodes
+  */
 
   return (
     <Animation type="fadeUp" delay={400}>
@@ -34,14 +65,14 @@ export const Blog = () => {
         <div className={classes.Blog}>
           <h1 className={classes.Title}>Latest Articles</h1>
           <div className={classes.Articles}>
-            {articles.map(article => (
-              <div key={article.frontmatter.slug} className={classes.Article}>
+            {externalArticles.map(article => (
+              <div key={article.url} className={classes.Article}>
                 <ArticleCard
-                  slug={article.frontmatter.slug}
-                  image={article.frontmatter.image}
-                  title={article.frontmatter.title}
-                  date={article.frontmatter.date}
-                  readingTime={article.frontmatter.readingTime}
+                  url={article.url}
+                  image={article.image}
+                  title={article.title}
+                  date={article.date}
+                  readingTime={article.readingTime}
                 />
               </div>
             ))}
