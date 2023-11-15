df09b2a349b1944065a9ac5faf17bdae66bb1450
davinci2015 <danijel.vincijanovic@gmail.com>
2023-11-15 08:51:17 +0100

    mixpanel

--- portfolio/src/components/Header/Header.tsx
+++ portfolio/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Logo, Button, CalendlyButton } from ".."
+import { Logo, CalendlyButton } from ".."
 
 import * as classes from "./style.module.css"
 
