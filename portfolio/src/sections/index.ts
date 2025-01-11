eaddba208dccf27054a751f1923dbfba539e2db5
Danijel Vincijanović <danijel.vincijanovic@gmail.com>
2025-01-11 14:12:44 +0100

    React Native app crash article (#50)

--- portfolio/src/sections/index.ts
+++ portfolio/src/sections/index.ts
@@ -1,6 +1,5 @@
 import { Hero } from "./Hero/Hero"
 import { Footer } from "./Footer/Footer"
 import { Blog } from "./Blog/Blog"
-import { References } from "./References/References"
 
-export { Hero, Footer, Blog, References }
+export { Hero, Footer, Blog }
