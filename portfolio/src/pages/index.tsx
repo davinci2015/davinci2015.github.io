8b99846e78fe16ecf0f4f87e3c47b43f5e2a78c1
davinci2015 <danijel.vincijanovic@gmail.com>
2025-01-11 14:24:08 +0100

    Minor style updates

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -3,10 +3,14 @@ import React from "react"
 import { Seo, Layout } from "../components"
 import { Blog, Hero } from "../sections"
 
+import * as classes from "./index.module.css"
+
 const IndexPage = () => (
   <Layout>
-    <Hero />
-    <Blog title="Latest Articles" maxArticlesToShow={3} />
+    <div className={classes.Container}>
+      <Hero />
+      <Blog title="Latest Articles" maxArticlesToShow={3} />
+    </div>
   </Layout>
 )
 
