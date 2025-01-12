0af4e05b20b045b6e91f37ac3ea87aab907aec27
davinci2015 <danijel.vincijanovic@gmail.com>
2025-01-12 13:03:21 +0100

    Removed AI badge

--- portfolio/src/blog/components/index.tsx
+++ portfolio/src/blog/components/index.tsx
@@ -4,6 +4,5 @@ import { Quote } from "./Quote/Quote"
 import { Intro } from "./Intro/Intro"
 import { Resources } from "./Resources/Resources"
 import { Divider } from "./Divider/Divider"
-import { AIBadge } from "./AIBadge/AIBadge"
 
-export { Pre, PhotoCaption, Intro, Quote, Resources, Divider, AIBadge }
+export { Pre, PhotoCaption, Intro, Quote, Resources, Divider }
